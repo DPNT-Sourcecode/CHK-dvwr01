3350ae1168803c19ae359491803162b498a352f6
sbx_user1059 <sbx_user1059@ip-10-11-35-185.eu-west-2.compute.internal>
2018-03-08 20:02:35 +0000

    Thu Mar 08 20:02:35 UTC 2018

---
+++ test/solutions/test-hello.js
@@ -0,0 +1,7 @@
+const test = require("ava");
+const hello = require("../../lib/solutions/hello");
+
+test("say hello to the world", t => {
+  const response = hello();
+  t.is(response, "Hello, world!");
+});
