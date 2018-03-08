8bba825abd4c004bee0b6ca470f8499ebc205349
sbx_user1059 <sbx_user1059@ip-10-11-35-185.eu-west-2.compute.internal>
2018-03-08 20:04:20 +0000

    Thu Mar 08 20:04:20 UTC 2018

--- test/solutions/test-hello.js
+++ test/solutions/test-hello.js
@@ -5,3 +5,8 @@ test("say hello to the world", t => {
   const response = hello();
   t.is(response, "Hello, World!");
 });
+
+test("say hello to your friend", t => {
+  const response = hello("John");
+  t.is(response, "Hello, John!");
+});
