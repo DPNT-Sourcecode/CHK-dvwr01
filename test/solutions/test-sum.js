b8a465c388b209c29310b87d9282b0737ee9826e
sbx_user1059 <sbx_user1059@ip-10-11-35-185.eu-west-2.compute.internal>
2018-03-08 19:59:23 +0000

    Thu Mar 08 19:59:23 UTC 2018

--- test/solutions/test-sum.js
+++ test/solutions/test-sum.js
@@ -1,6 +1,6 @@
-var sum = require('../../lib/solutions/sum');
+const test = require("ava");
+var sum = require("../../lib/solutions/sum");
 
-exports['compute sum'] = function (test) {
-    test.equal(sum(1, 2), 3);
-    test.done();
-};
+test("compute sum", t => {
+  t.is(sum(1, 2), 3);
+});
